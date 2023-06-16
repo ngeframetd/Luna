d30a498273ce0de0d6f6f72d2da39a134b595f94
ngeframetd <70860536+ngeframetd@users.noreply.github.com>
2023-06-17 02:50:58 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -277,7 +277,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, dest_id):
         file_metadata = {
             "name": directory_name,
-            "description": f'Uploaded by {self.__listener.message.from_user.id}',
+            "description": directory_name,
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if dest_id is not None:
@@ -297,7 +297,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': f'Uploaded by {self.__listener.message.from_user.id}',
+            'description': file_name,
             'mimeType': mime_type,
         }
         if dest_id is not None:
